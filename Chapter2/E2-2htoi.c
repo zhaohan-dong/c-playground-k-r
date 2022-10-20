bf25e2bc0bb48f1882719a7c4397ebdb3d9cab64
Zhaohan Dong <zhaohan_dong@CR-3-Flt-Computer.local>
2022-10-20 19:57:28 +0000

    chapter 2

--- Chapter2/E2-2htoi.c
+++ Chapter2/E2-2htoi.c
@@ -1,7 +1,38 @@
 /* page 42 htoi(s) */
 
 #include<stdio.h>
+#include<ctype.h>
+
+int htoi(s)
+char s[];
+{
+    int i, n; // n output number
+    n = 0;
+    for (i = 0;
+            (s[i] >= '0' && s[i] <= '9') ||
+            (s[i] >= 'a' && s[i] <= 'f') || 
+            (s[i] >= 'A' && s[i] <= 'F');
+            ++i) {
+                n = 16 * n; // move right one digit
+                if (s[i] >= '0' && s[i] <= '9') { // case digit
+                    n = n + s[i] - '0';
+                } else if (s[i] >= 'a' && s[i] <= 'f') { // case lowercase
+                    n = n + s[i] - 'W';
+                } else {
+                    n = n + s[i] - '7';
+                }
+            }
+    return n;
+}
 
 int main() {
-    
+    int c, i = 0;
+    char arr;
+    int d;
+    while ((c = getchar()) != EOF && i < 256) {
+        arr[i++] = c;
+    }
+    d = htoi(arr);
+    printf("%d\n", d);
 }
+

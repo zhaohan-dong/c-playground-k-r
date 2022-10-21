804730b21a714ad728e1f098820217c80cc44b9c
Zhaohan Dong <zhaohan_dong@CR-3-Flt-Computer.local>
2022-10-21 15:29:04 +0000

    E2-5 complete

--- Chapter2/E2-4alternatesqueeze.c
+++ Chapter2/E2-4alternatesqueeze.c
@@ -6,8 +6,8 @@ void squeeze(char s1[], char s2[])
 {
     int i, j, k; // i, j for looping through s1, k, l for looping through s2
 
-	for (i = j = 0; s1[i] != '\0'; ++i) {
-		for (k = 0; s2[k] != '\0'; ++k) {
+	for (i = j = 0; s1[i] != '\0'; i++) {
+		for (k = 0; s2[k] != '\0'; k++) {
 			if (s1[i] != s2[k])
 				s1[j++] = s1[i];
 		}
@@ -17,5 +17,6 @@ void squeeze(char s1[], char s2[])
 
 
 int main() {
+	squeeze("Hello world\0", "ol\0");
 	return 0;
 }
---
+++ Chapter2/E2-5any.c
@@ -0,0 +1,26 @@
+/* any(s1,s2) that returns first index in s1 where any char from s2 occurs, or -1 if doesn't contain any*/
+
+#include<stdio.h>
+
+int any(char s1[], char s2[]);
+
+int main(void) {
+    int loc;
+    loc = any("Hello world", "ol");
+    printf("%d\n", loc);
+    return 0;
+}
+
+int any(char s1[], char s2[]) {
+    int i, j;
+    int firstloc = -1; // set default return to -1
+
+    for(i = 0; s1[i] != '\0'; ++i) { // loop through every position of s1
+        for(j = 0; s2[j] != '\0' && firstloc == -1; ++j) { // loop through s2 for each location
+            if (s1[i] == s2[j])
+                firstloc = i; // match found and updates first loc
+        }
+    }
+
+    return firstloc;
+}

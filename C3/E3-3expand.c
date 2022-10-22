56bfdd310524207ae9fc9e5b13f6ed306f70bea5
Zhaohan Dong <zhaohan_dong@CR3-Flt-Computer.local>
2022-10-22 20:50:06 +0000

    E3-3 complete

--- C3/E3-3expand.c
+++ C3/E3-3expand.c
@@ -8,11 +8,11 @@ int expand(char * s1, char * s2);
 
 int main(void)
 {
-    char s1[256] = "a-c", s2[256];
+    char s1[256] = "a-c1-5 1256-9", s2[256];
     int r;
     r = expand(s1, s2);
     printf("%s\nReturn code: %d\n", s2, r);
-    return 0;
+    return r;
 }
 
 int expand(char * s1, char * s2)
@@ -21,11 +21,10 @@ int expand(char * s1, char * s2)
     j = 0;
 
     for (i = 0; s1[i] != '\0'; ++i) {
-
-        // if i is start of shorthand
+        // if i is start of shorthand (if s1[i] is alphanumeric and the following space is -)
         if (isalnum(s1[i]) && (s1[i+1] == '-')) {
             
-            // if i and i+2 is are same type
+            // if i and i+2 is are same type, else throw error
             if ((isalpha(s1[i]) && isalpha(s1[i+2])) || (isnumber(s1[i]) && isnumber(s1[i+2]))) {
 
                 // parse s1[i] to s2[j] (start of sequence)
@@ -33,12 +32,18 @@ int expand(char * s1, char * s2)
 
                 while (s2[j] != s1[i+2]) {
                     // determine if the sequence in s1 increases or decreases
-                    s2[++j] = s2[j] + (s1[i] < s1[i+2]) ? 1 : -1;
+                    s2[++j] = s2[j] + ((s1[i] < s1[i+2])? 1 : -1);
                 }
+                ++i;
             } else {
+                // throw error because s1[i] and s1[i+2] are not same type
                 return -1;
             }
-        } 
+        } else {
+            // not shorthand start, pass on s1[i] to s2[j] and increase j for next iteration (i increased at for-loop)
+            s2[j++] = s1[i];
+        }
     }
+    s2[j] = '\0';
     return 0;
 }

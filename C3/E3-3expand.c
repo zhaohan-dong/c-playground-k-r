e0256a6036c3c9fed4c85082e40c8e7e0990af52
Zhaohan Dong <zhaohan_dong@CR3-Flt-Computer.local>
2022-10-22 20:55:22 +0000

    E3-3 complete

--- C3/E3-3expand.c
+++ C3/E3-3expand.c
@@ -8,11 +8,11 @@ int expand(char * s1, char * s2);
 
 int main(void)
 {
-    char s1[256] = "a-c1-5 1256-9", s2[256];
+    char s1[256] = "-a-c1-5 -124-6-99-3a-d -d-f", s2[256];
     int r;
     r = expand(s1, s2);
     printf("%s\nReturn code: %d\n", s2, r);
-    return r;
+    return 0;
 }
 
 int expand(char * s1, char * s2)
@@ -24,16 +24,19 @@ int expand(char * s1, char * s2)
         // if i is start of shorthand (if s1[i] is alphanumeric and the following space is -)
         if (isalnum(s1[i]) && (s1[i+1] == '-')) {
             
-            // if i and i+2 is are same type, else throw error
+            // if i and i+2 is are same type, this is shorthand, else throw error
             if ((isalpha(s1[i]) && isalpha(s1[i+2])) || (isnumber(s1[i]) && isnumber(s1[i+2]))) {
 
                 // parse s1[i] to s2[j] (start of sequence)
                 s2[j] = s1[i];
-
+                
+                // when end of s2 doesn't match end of shorthand (s1[i+2]) yet, put another char into s2
                 while (s2[j] != s1[i+2]) {
                     // determine if the sequence in s1 increases or decreases
                     s2[++j] = s2[j] + ((s1[i] < s1[i+2])? 1 : -1);
                 }
+
+                // increase i so we move past the '-' in shorthand
                 ++i;
             } else {
                 // throw error because s1[i] and s1[i+2] are not same type
@@ -44,6 +47,6 @@ int expand(char * s1, char * s2)
             s2[j++] = s1[i];
         }
     }
-    s2[j] = '\0';
+    s2[j] = '\0'; // terminate the string
     return 0;
 }

9887f7ef2d5416a99995dca3274c9549e4c7ebde
Zhaohan Dong <zhaohan_dong@CR3-Flt-Computer.local>
2022-10-23 22:38:13 +0000

    e3-5

---
+++ C3/E3-5itob.c
@@ -0,0 +1,32 @@
+/* E3-5 itob(n,s,b) that converts the integer n into a base b character representation in the string s. */
+
+#include<stdio.h>
+#include <string.h>
+#include "reverse.c"
+
+int itob(int n, char s[], int b) {
+    int i, t, remainder;
+    t = n;
+
+    for (i = 0; t != 0; ++i) {
+        // go through the int from lower to higher digits, the remainder is the digit
+        remainder = t % b;
+        if (remainder < 10) {
+            // digit less than 11 (numbers)
+            s[i] = (char) (remainder + 48);
+        } else {
+            // digit greater than 11 (alphabet required)
+            s[i] = (char) (remainder + 55);
+        }
+        t = (t - remainder) / b;
+    }
+    reverse(s);
+    s[++i] = '\0';
+    return 0;
+}
+
+int main(void) {
+    char out[256];
+    itob(15697, out, 16);
+    printf("%s\n", out);
+}

ed19d751abf1874b7176290d16ef7909f5e31a46
Zhaohan Dong <zhaohan_dong@CR3-Flt-Computer.local>
2022-10-24 23:06:14 +0000

    E4-2

--- C4/E4-2natof.c
+++ C4/E4-2natof.c
@@ -2,10 +2,13 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <math.h>
 
 double natof(char s[]) {
     double val, power;
-    int i, sign;
+    int i, sign, exp, exp_sign;
+    exp = 0;
+    exp_sign = 1;
 
     for (i = 0; isspace(s[i]); i++)  /* skip white space */
         ;
@@ -20,9 +23,24 @@ double natof(char s[]) {
         val = 10.0 * val + (s[i] - '0');
         power *= 10.0;
     }
-    return sign * val / power;
+
+    // Added section for scientific notation
+    if (s[i] == 'e' || s[i] == 'E') {
+        i++;
+        if (!isdigit(s[i]))
+            exp_sign = (s[i] == '-') ? -1 : 1;
+        if (s[i] == '+' || s[i] == '-')
+            i++; 
+        for (exp = 0; isdigit(s[i]); i++)
+            exp = 10 * exp + (s[i] - '0');
+    }
+
+    return sign * val / power * pow(10, exp_sign * exp);
 }
 
 int main() {
+    double f;
+    f = natof("123.45e-6");
+    printf("%f\n", f);
     return 0;
 }

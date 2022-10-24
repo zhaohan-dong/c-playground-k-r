7ef22ca36ae3b1340b0f359a190226aa3ec9e65c
Zhaohan Dong <zhaohan_dong@CR3-Flt-Computer.local>
2022-10-24 22:49:12 +0000

    added E4-2

---
+++ C4/E4-2natof.c
@@ -0,0 +1,28 @@
+/* E4-2 extended atof to handle scientific notation of 123.45e-6 */
+
+#include <stdio.h>
+#include <ctype.h>
+
+double natof(char s[]) {
+    double val, power;
+    int i, sign;
+
+    for (i = 0; isspace(s[i]); i++)  /* skip white space */
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++; 
+    for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i] - '0');
+    if (s[i] == '.')
+        i++;
+    for (power = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    return sign * val / power;
+}
+
+int main() {
+    return 0;
+}

0087c920bd294d186a519770957b5a34b811d275
Zhaohan Dong <zhaohan_dong@CR3-Flt-Computer.local>
2022-10-25 15:29:32 +0000

    e4-3

---
+++ C4/E4-3calc.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>   /* for atof() */
+#include "getop.c"
+#include "push.c"
+#include "pop.c"
+#include "getch.c"
+#include "ungetch.c"
+
+#define MAXOP   100   /* max size of operand or operator */
+#define NUMBER  '0'   /* signal that a number was found */
+
+int getop(char []);
+void push(double);
+double pop(void);
+
+/* reverse Polish calculator */
+main()
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(pop() / op2);
+            else
+                printf("error: zero divisor\n");
+            break;
+        case '%':
+            op2 = pop();
+            if (op2 != 0.0)
+                push((int) pop() % (int) op2);
+            else
+                printf("error: zero divisor\n");
+            break;
+
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+        default:
+            printf("error: unknown command %s\n", s);
+            break;
+        }
+    }
+    return 0;
+}

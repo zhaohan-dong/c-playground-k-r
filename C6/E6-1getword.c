5d1917ee66be9f439d39af404f98fc21e07acc09
Zhaohan Dong <zhaohan_dong@CR3-Flt-Computer.local>
2022-11-10 20:20:10 +0000

    E6-1 with underscore

---
+++ C6/E6-1getword.c
@@ -0,0 +1,26 @@
+/* exercise getword:  get next word or character from input
+handle underscores, string constants, comments or preprocessor control lines */
+#include <stdio.h>
+
+int getword(char *word, int lim)
+{
+    int c, getch(void);
+    void ungetch(int);
+    char *w = word;
+
+    while (isspace(c = getch()))
+        ;
+    if (c != EOF)
+        *w++ = c;
+    if (!isalpha(c) && !strcmp(c, '_')) {
+        *w = '\0';
+        return c;
+    }
+    for ( ; --lim > 0; w++)
+        if (!isalnum(*w = getch())) {
+            ungetch(*w);
+            break;
+        }
+    *w = '\0';
+    return word[0];
+}
